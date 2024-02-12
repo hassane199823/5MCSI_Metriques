b49a8e4c3913ed63941acd16b65468a55e4718e4
hassane199823 <159788282+hassane199823@users.noreply.github.com>
2024-02-12 15:50:01 +0100

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -13,8 +13,6 @@ def MaPremiereAPI():
 @app.route('/')
 def hello_world():
     return render_template('hello.html') #comm2
-@app.route("/contact/")
-def MaPremiereAPI():
-    return "<h2>Ma page de contact</h2>"  
+  
 if __name__ == "__main__":
   app.run(debug=True)
