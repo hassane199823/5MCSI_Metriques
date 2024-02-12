4fbc4365198256d895218a06613b0d47b035178b
hassane199823 <159788282+hassane199823@users.noreply.github.com>
2024-02-12 17:02:42 +0100

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -5,59 +5,15 @@ from datetime import datetime
 from urllib.request import urlopen
 import sqlite3
                                                                                                                                        
-app = Flask(__name__)  
-[16:50] Ines HOUFANI
-from flask import Flask, render_template_string, render_template, jsonify
-
-from flask import render_template 
-
-from flask import json
-
-from datetime import datetime
-
-from urllib.request import urlopen
-
-import sqlite3
-
-app = Flask(__name__)                                                                                                                  
+app = Flask(_name_)                                                                                                                  
+                                                                                                                                       
+@app.route("/contact/")
+def contact():
+    return render_template("contact.HTML")
 
 @app.route('/')
-
 def hello_world():
-
-      return render_template('hello.html') #inesHoufani
- 
-@app.route('/contact/')
-
-def contact():
-
-      return render_template('contact.HTML')
- 
-@app.route('/paris/')
-
-def meteo():
-
-    response = urlopen('https://api.openweathermap.org/data/2.5/forecast/daily?q=Paris,fr&cnt=16&appid=bd5e378503939ddaee76f12ad7a97608')
-
-    raw_content = response.read()
-
-    json_content = json.loads(raw_content.decode('utf-8'))
-
-    results = []
-
-    for list_element in json_content.get('list', []):
-
-        dt_value = list_element.get('dt')
-
-        temp_day_value = list_element.get('temp', {}).get('day') - 273.15 # Conversion de Kelvin en °c 
-
-        results.append({'Jour': dt_value, 'temp': temp_day_value})
-
-    return jsonify(results=results)  
-
-if __name__ == "__main__":
-
-  app.run(debug=True)
+    return render_template('hello.html')
 
 @app.route('/paris/')
 def meteo():
@@ -70,11 +26,10 @@ def meteo():
         temp_day_value = list_element.get('temp', {}).get('day') - 273.15 # Conversion de Kelvin en °c 
         results.append({'Jour': dt_value, 'temp': temp_day_value})
     return jsonify(results=results)
-                                                                                                                                       
-@app.route('/')
-def hello_world():
-    return render_template('hello.html') #comm2
-  
-if __name__ == "__main__":
-  app.run(debug=True)
 
+@app.route("/rapport/")
+def mongraphique():
+    return render_template("graphique.html")
+  
+if _name_ == "_main_":
+  app.run(debug=True)
